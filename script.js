1ea349e5e6130c3007997306f533138400f0a8af
Selasi Dzeamesi <110040901+icybearfvreal@users.noreply.github.com>
2022-10-03 23:20:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function dropMenu() {
+    document.getElementById('dropMenu').style.opacity = '1'
+//    
+//    document.getElementById('dropMenu').style.transition = '0.5s ease-in-out'
+}
+ 
+function menuUp() {
+    document.getElementById('dropMenu').style.opacity = '0'
+}
+
+//function menuDrop() {
+//    document.getElementById('dropMenu').style.visibility = 'hidden';
+//    
+//    document.getElementById('dropMenu').style.transition = '0.5s ease-in-out'
+//}
+
+function apGet() {
+    document.getElementById('pin').style.opacity = '1'
+}
+
+function apTarget() {
+    document.getElementById('targ').style.opacity = '1'
+}
+
